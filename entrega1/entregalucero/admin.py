362de1176191e701e4d077d4e7478fdb3082d7fb
polillalucero <paulalucero85@gmail.com>
2022-06-08 10:16:14 -0300

    Mi primera version

---
+++ entrega1/entregalucero/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import  Tutorx, Mascota, atencionprofesional
+
+# Register your models here.
+admin.site.register(Tutorx)
+admin.site.register(Mascota)
+admin.site.register(atencionprofesional)
---
+++ entrega1/entregalucero/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Tutorx(models.Model):
+    Nombre = models.CharField(max_length=50)
+    Apellido = models.CharField(max_length=50)
+    Ocupacion = models.CharField(max_length=50)
+    Edad = models.IntegerField()
+    nombre_mascota = models.CharField(max_length=50)
+
+class Mascota(models.Model):
+    Nombre = models.CharField(max_length=50)
+    Nombre_tutorx = models.CharField(max_length=50)
+    Especie = models.CharField(max_length=50)
+    Raza = models.CharField(max_length=50)
+    Vacunas = models.CharField(max_length=50)
+    Edad = models.IntegerField()
+    Peso = models.IntegerField()
+
+class atencionprofesional(models.Model):
+     Nombre_profesional= models.CharField(max_length=50)
+     Motivo_de_atencion = models.CharField(max_length=200)
+     Proxima_visita = models.DateField()
